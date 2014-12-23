1c4987f3805432ed7f260f5d5a7d1d6546a7cc61
Philip Taffner <philip.taffner@bluegfx.de>
2014-12-23 10:35:04 +0100

    Use libnotify instead of osd_cat.
    Change-Id: Ida38b1dc927faa53404ad674710d6bf35bca74ac

--- osd_volume.py
+++ osd_volume.py
@@ -7,27 +7,56 @@
 # 2014 - Philip Taffner <philip.taffner@bluegfx.de>
 #
 
-import sys, os, re
+import sys, os, re, pynotify
 from subprocess import check_output, call
 
-if len(sys.argv) < 2:
-	print "Usage: %s <percentage(+|-)>|m"
-	sys.exit(1)
-
-if sys.argv[1] == "m":
+def toggleMute():
 	res = check_output("amixer -D pulse set Master toggle".split(" "))
 	m = re.compile("\[(on|off)\]").search(res)
-	status = "Muted" if m.group(1) == "off" else "Unmuted"
 
-	os.system("killall osd_cat")
-	os.system("echo %s | osd_cat -d 2 -A center -p middle -c lightblue -s 2 -f -adobe-helvetica-bold-r-*-*-34-*-*-*-*-*-*-*" % status)
-else:
-	res = check_output(("amixer set Master %s" % sys.argv[1]).split(" "))
+	if m.group(1) == "on":
+		setVolume("0+")
+		return
+
+	os.system("killall notify-osd");
+	if not pynotify.init("osd_volume"):
+		sys.exit(1)
+
+	n = pynotify.Notification("Lautstärke", "", "audio-volume-muted")
+	n.set_hint("x-canonical-private-synchronous", "")
+	n.set_hint_int32("value", 0)
+
+	if not n.show():
+		print "Failed to send notification"
+		sys.exit(1)
+
+def setVolume(value):
+	res = check_output(("amixer set Master %s" % value).split(" "))
 	m = re.compile("\[(.+)%\]").search(res)
 
 	if m:
-		os.system("killall osd_cat")
-		call(("osd_cat -d 2 -A center -p middle -c lightblue -b percentage -T Lautstärke -s 2 -f -adobe-helvetica-bold-r-*-*-34-*-*-*-*-*-*-* -P %s" % m.group(1)).split(" "))
+		os.system("killall notify-osd");
+		if not pynotify.init("osd_volume"):
+			sys.exit(1)
+
+		n = pynotify.Notification("Lautstärke", "", "audio-volume-high")
+		n.set_timeout(pynotify.EXPIRES_DEFAULT)
+		n.set_hint("x-canonical-private-synchronous", "")
+		n.set_hint_int32("value", int(m.group(1)))
+
+		if not n.show():
+			print "Failed to send notification"
+			sys.exit(1)
 	else:
 		sys.stderr.write("Error: Could not read current volume level.\n")
 		sys.exit(1)
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print "Usage: %s <percentage(+|-)>|m"
+		sys.exit(1)
+
+	if sys.argv[1] == "m":
+		toggleMute()
+	else:
+		setVolume(sys.argv[1])
